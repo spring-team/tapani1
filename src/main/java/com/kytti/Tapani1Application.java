3c6220034b7ae411539da6e65d7ca18782b3a288
Rod Johnson <johnsonroda@gmail.com>
2018-02-19 19:10:32 +1100

    Initial commit from Atomist

---
+++ src/main/java/com/kytti/Tapani1Application.java
@@ -0,0 +1,12 @@
+package com.kytti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tapani1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tapani1Application.class, args);
+	}
+}
